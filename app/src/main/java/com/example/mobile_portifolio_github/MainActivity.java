6367c254af64b327cdfaf55cf9bda744c713bd1b
jamesw <jamersonwalderson@gmail.com>
2020-06-11 14:01:30 -0300

    init: projeto funcional

---
+++ app/src/main/java/com/example/mobile_portifolio_github/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.mobile_portifolio_github;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = (WebView) findViewById(R.id.myWebView);
+        // Ativa o javasript
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("http://jamersonwalderson.github.io");
+
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return false;
+
+        }
+    }
+
+}
